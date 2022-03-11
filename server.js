f21cbdb724f5228867fe456de05cfa71cf1b58b3
Jan Niklas Pudschun <pudschun.j.n@gmx.de>
2022-03-11 15:28:45 +0100

    fixing Post anchor so not all Pokemon have to be created previosly

--- server.js
+++ server.js
@@ -51,24 +51,60 @@ app.get("/leaderboard/:name", (req, res) => {
     Pokemon.find({name: req.params.name}, (err, data) => res.send(data));
   });
 
-app.post("/leaderboard/:name", (req, res) => {
-    Pokemon.create({
-      name: req.params.name,
-      wins: 0,
-      losses: 0,
-    }).then(function (newPokemon) {
-      res.send(newPokemon);
-    });
+app.post("/leaderboard/:name/:result",(req, res) => {
+    Pokemon.exists({name:req.params.name},(error, result)=>{
+        if (error){
+            console.log(error)
+        } else {
+            
+              console.log("result:", JSON.stringify(result).length)  //result is true if myData already exists
+            
+         
+    
+    
+    if(JSON.stringify(result).length > 4){
+        if(req.params.result === "win"){
+            Pokemon.updateOne({name: req.params.name},{$inc:{wins:1}}).then(function (updatePokemon) {
+                res.send(updatePokemon);
+            });
+        } 
+        if(req.params.result === "loss"){
+            Pokemon.updateOne({name: req.params.name},{$inc:{losses:1}}).then(function (updatePokemon) {
+                res.send(updatePokemon);
+            });
+        }
+    } else {
+        if(req.params.result === "win"){
+        Pokemon.create({
+            name: req.params.name,
+            wins: 1,
+            losses: 0,
+          }).then(function (newPokemon) {
+            res.send(newPokemon);
+          });
+        }
+        if(req.params.result === "loss"){
+            Pokemon.create({
+                name: req.params.name,
+                wins: 0,
+                losses: 1,
+              }).then(function (newPokemon) {
+                res.send(newPokemon);
+              });
+            }
+    }
+
+    } })
   });
 
 app.put("/leaderboard/:name/:result",(req,res)=>{
     
     if(req.params.result === "win"){
-        Pokemon.updateOne({name: req.params.name},{$inc:{wins:1}}).then(function (updatePokemon) {
+        Pokemon.updateOne({name: req.params.name},{name:req.params.name,$inc:{wins:1},losses:losses},{upsert:true}).then(function (updatePokemon) {
             res.send(updatePokemon);
         });
     } else {
-        Pokemon.updateOne({name: req.params.name},{$inc:{losses:1}}).then(function (updatePokemon) {
+        Pokemon.updateOne({name: req.params.name},{$inc:{losses:1}},{upsert:true}).then(function (updatePokemon) {
             res.send(updatePokemon);
         });
     }
